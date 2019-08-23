f83758d24374131fadf028d25ac787b088b5a972
lorecast162 <lorecastcauli@gmail.com>
2019-08-23 15:55:16 +0200

    commit

--- config.el
+++ config.el
@@ -1,4 +1,17 @@
-(add-hook 'after-init-hook '(setq make-backup-file-name-function 'lorecast162/my-backup-file-name))
+(defun my-backup-file-name (fpath)
+  "Return a new file path of a given file path.
+If the new path's directories does not exist, create them."
+  (let* (
+         (backupRootDir "~/.emacs.d/backup/")
+         (filePath (replace-regexp-in-string "[A-Za-z]:" "" fpath )) ; remove Windows driver letter in path, for example, “C:”
+         (backupFilePath (replace-regexp-in-string "//" "/" (concat backupRootDir filePath "~") ))
+         )
+    (make-directory (file-name-directory backupFilePath) (file-name-directory backupFilePath))
+    backupFilePath
+    )
+  )
+
+(setq make-backup-file-name-function 'my-backup-file-name)
 (setq auto-save-default nil)
 
 (add-hook 'after-init-hook 'spacemacs/toggle-menu-bar-on)
